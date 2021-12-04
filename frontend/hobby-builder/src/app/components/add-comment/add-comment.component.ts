573da212ba32ea46235ca6517315e49a36e43559
richardpham1998 <34046976+richardpham1998@users.noreply.github.com>
2021-12-04 14:40:47 -0800

    Add-Comment Form Update

--- frontend/hobby-builder/src/app/components/add-comment/add-comment.component.ts
+++ frontend/hobby-builder/src/app/components/add-comment/add-comment.component.ts
@@ -7,6 +7,7 @@ import { EventService } from 'src/app/services/event.service';
 import { NotificationService } from 'src/app/services/notification.service';
 import { PostService } from 'src/app/services/post.service';
 import { UserService } from 'src/app/services/user.service';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-add-comment',
@@ -17,6 +18,8 @@ export class AddCommentComponent implements OnInit {
   user: User;
   eventOrPost: Number;
   idToCommentOn: String;
+
+  commentForm: FormGroup;
 
   content: String = '';
   userId: String = null;
@@ -30,7 +33,6 @@ export class AddCommentComponent implements OnInit {
 
   profileObject: any = null;
 
-  blankContent: boolean = false;
   added: boolean = false;
 
   commentId: String;
@@ -42,7 +44,8 @@ export class AddCommentComponent implements OnInit {
     private route: ActivatedRoute,
     private postService: PostService,
     private eventService: EventService,
-    private notificationService: NotificationService
+    private notificationService: NotificationService,
+    private fb: FormBuilder
   ) {}
 
   ngOnInit(): void {
@@ -55,13 +58,19 @@ export class AddCommentComponent implements OnInit {
         6,
         this.profileObject.sub.length
       );
-      this.userService
-        .getUser(this.userId)
-        .subscribe((user) => (this.user = user));
+
+      this.commentForm = this.fb.group({
+        content: ['', Validators.required],
+        userId: this.userId,
+      });
+
+      this.userService.getUser(this.userId).subscribe((user) => {
+        this.user = user;
+      });
     });
   }
 
-  addComment(postOrEventOrProfile: Number, postEventId: String) {
+  onSubmit(): void {
     //indicator to link to comment
     var postId: String = null;
     var eventId: String = null;
@@ -72,97 +81,78 @@ export class AddCommentComponent implements OnInit {
     var userToNotify: String = null;
 
     //0
-    if (postOrEventOrProfile == 0) {
-      postId = postEventId;
+    if (this.eventOrPost == 0) {
+      postId = this.idToCommentOn;
       link = 'post';
 
-      this.postService.getPost(this.idToCommentOn).subscribe(
-        post=>
-        {
-          userToNotify = post.user;
-          this.completeComment(link, userToNotify, profileId, postId, eventId);
-        }
-      )
+      this.postService.getPost(this.idToCommentOn).subscribe((post) => {
+        userToNotify = post.user;
+        this.completeComment(link, userToNotify, profileId, postId, eventId);
+      });
     }
     //1
-    else if (postOrEventOrProfile == 1) {
-      eventId = postEventId;
+    else if (this.eventOrPost == 1) {
+      eventId = this.idToCommentOn;
       link = 'event';
 
-      this.eventService.getEvent(this.idToCommentOn).subscribe(
-        event=>
-        {
-          userToNotify = event.user;
-          this.completeComment(link, userToNotify, profileId, postId, eventId);
-        }
-      )
+      this.eventService.getEvent(this.idToCommentOn).subscribe((event) => {
+        userToNotify = event.user;
+        this.completeComment(link, userToNotify, profileId, postId, eventId);
+      });
     }
     //2
-    else if (postOrEventOrProfile == 2) {
-      profileId = postEventId;
+    else if (this.eventOrPost == 2) {
+      profileId = this.idToCommentOn;
       link = 'profile';
 
-      this.userService.getUser(this.idToCommentOn).subscribe(
-        user=>
-        {
-          userToNotify = user._id;
-          this.completeComment(link, userToNotify, profileId, postId, eventId);
-        }
-      )
+      this.userService.getUser(this.idToCommentOn).subscribe((user) => {
+        userToNotify = user._id;
+        this.completeComment(link, userToNotify, profileId, postId, eventId);
+      });
     }
   }
 
   //completes the process of adding comment
-  completeComment(link: String, userToNotify: String, profileId:String, postId:String, eventId: String)
-  {
-    if (this.content != null) {
-      this.content.trim;
+  completeComment(
+    link: String,
+    userToNotify: String,
+    profileId: String,
+    postId: String,
+    eventId: String
+  ) {
+    if (this.commentForm.value.content != null) {
+      this.commentForm.value.content.trim;
     }
 
-    if (this.content == null || this.content == '') {
-      this.blankContent = true;
-      this.added = false;
-    } else {
-      const newNotification = {
-        text: this.user.username + ' commented on your ' + link + '.',
-        linkType: link,
-        user: userToNotify, //person who created the post/event/profile
-        idToLink: this.idToCommentOn, //post/event/profile id
-        date_created: new Date(),
-        date_modified: null,
-        newNotif: true
-      };
+    const newNotification = {
+      text: this.user.username + ' commented on your ' + link + '.',
+      linkType: link,
+      user: userToNotify, //person who created the post/event/profile
+      idToLink: this.idToCommentOn, //post/event/profile id
+      date_created: new Date(),
+      date_modified: null,
+      newNotif: true,
+    };
 
-      const newComment = {
-        content: this.content,
-        user: this.userId,
-        author: this.user.username,
-        likes: this.likes,
-        email: this.profileObject.name,
-        profile: profileId,
-        post: postId,
-        event: eventId,
-        date_created: new Date(),
-        date_modified: null
-      };
+    const newComment = {
+      content: this.commentForm.value.content,
+      user: this.commentForm.value.userId,
+      author: this.user.username,
+      likes: this.likes,
+      email: this.profileObject.name,
+      profile: profileId,
+      post: postId,
+      event: eventId,
+      date_created: new Date(),
+      date_modified: null,
+    };
 
-      this.commentService.addComment(newComment).subscribe((comment) => {
-        //create notification
-        this.notificationService.addNotification(newNotification).subscribe();
-      });
+    this.commentService.addComment(newComment).subscribe((comment) => {
+      //create notification
+      this.notificationService.addNotification(newNotification).subscribe();
+    });
 
-      this.content = '';
-      this.userId = null;
-      this.email = null;
-      this.event = null;
-      this.post = null;
-      this.date_created = null;
-      this.date_modified = null;
-      this.date_created = null;
-      this.date_modified = null;
 
-      this.blankContent = false;
-      this.added = true;
-    }
+    this.added = true;
   }
 }
